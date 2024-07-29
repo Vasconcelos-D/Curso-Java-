e3937124eb94ee3119a67077831305a87ba52bbd
Vasconcelos-D <roronoazoroaf@gmail.com>
2024-07-29 14:33:27 -0300

    set, tres oprs

--- hashCode_equals/src1_set/application/main.java
+++ hashCode_equals/src1_set/application/main.java
@@ -1,29 +1,31 @@
 package application;
 
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class main {
 
 	public static void main(String[] args) {
 
-		//Set<String> set = new HashSet<>();
-		//HashSet, é o mais rapido, mas não garante a ordem.
-		//Set<String> set = new TreeSet<>();
-		//Ele segue a ordem.
-		Set<String> set = new LinkedHashSet<>();
-		//Mamtem a ordem na qual os elemento são inseridos
-		set.add("Tv");
-		set.add("Notebook");
-		set.add("Tablet");
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));	
 		
-	
-		//set.remove("Tablet");// remove item do conjuto.
-		//set.removeIf(x -> x.charAt(0) == 'T');// remove usando um predicado, um filtro
+		//union
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
 		
+		//intersection
 		
-		for (String p : set) {
-			System.out.println(p);
-		}
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+		
+		//difference
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
+		
 	}
 }
